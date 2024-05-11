b3e137049cba9691a5fb64bdfd9a5c5dceaebc04
iboughtbed <testpythontosanzhar@gmail.com>
2024-05-11 20:53:30 +0500

    feat(auth): add clerk feat: add shadcn-ui refactor: add site config and metadata

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+const links = {
+  github: "https://github.com/iboughtbed/jotai-flow",
+};
+
+export const siteConfig = {
+  name: "NIS Insights",
+  url: "",
+  ogImage: "",
+  description: "A habit app",
+  links,
+};
+
+export type SiteConfig = typeof siteConfig;
